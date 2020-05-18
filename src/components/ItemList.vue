bf17f8825ac597b3cca5b41c540aed6eb21c9523
wzqup <869612826@qq.com>
2020-05-18 14:21:27 +0800

    完成静态页面

---
+++ src/components/ItemList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="memos" class="container">
+    <MemoItem v-for="(item,index) in [1,1,1,1,1]" :key="index"/>
+  </div>
+</template>
+
+<script lang='ts'>
+//1.导入 vue
+import { Vue, Component } from "vue-property-decorator";
+import MemoItem from "./MemoItem.vue";
+
+//2.编写组件类
+@Component({
+  components: {
+    MemoItem
+  }
+})
+export default class ItemList extends Vue {
+
+}
+</script>
---
+++ src/components/MemoItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="memo-container">
+    <div class="memo">
+      <div class="mark"></div>
+      <div class="memo-heading">
+        <h5 class="title">便签标题</h5>
+        <ul class="tools">
+          <li class="edit"></li>
+          <li class="delete"></li>
+        </ul>
+      </div>
+      <h6 class="memo-info">
+        <span class="timeStamp">2020/5/18</span>
+        <span class="category">分类:生活</span>
+      </h6>
+      <div class="content">
+        <div class="text">便签内容</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class MemoItem extends Vue {
+
+}
+</script>
